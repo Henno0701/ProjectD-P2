a5b85023d66bdf9308ae85de3a277077bfe16ab1
Cabonari <114401343+Cabonari@users.noreply.github.com>
2024-03-11 16:24:42 +0100

    added navigation bar

--- prototype2/lib/main.dart
+++ prototype2/lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
@@ -20,47 +19,81 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: Scaffold(
-        backgroundColor: const Color(0xFF121212),
-        body: Padding(
-          padding: const EdgeInsets.only(top: 50.0, left: 20.0), // Adjust left padding as needed
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start, // Align children to the start (left) of the row
-            children: [
-              const Text(
-                'Welcome',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  // ignore: unused_field
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home Page'),
+    Text('Make Reservation Page'),
+    Text('View Reservations Page'),
+    Text('Profile Page'),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF121212),
+      body: Padding(
+        padding: const EdgeInsets.only(
+            top: 50.0, left: 20.0), // Adjust left padding as needed
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment
+              .start, // Align children to the start (left) of the row
+          children: [
+            const Text(
+              'Welcome',
+              style: TextStyle(
+                color: Color(0xFF1E80ED),
+                fontSize: 25,
+              ),
+            ),
+            const Text(
+              'John',
+              style: TextStyle(
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontSize: 20,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 15.0),
+              child: Text(
+                'Quick Access',
                 style: TextStyle(
-                  color: Color(0xFF1E80ED),
-                  fontSize: 25,
+                  color: Color(0xFFB0B0B0),
+                  fontSize: 10,
                 ),
               ),
-              const Text(
-                'John',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 20,
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 15.0,),
-                child: Text(
-                  'Quick Access',
-                  style: TextStyle(
-                    color: Color(0xFFB0B0B0),
-                    fontSize: 10,
-                  ),
-                ),
-              ),
-              Padding(
-              padding: const EdgeInsets.only(top: 5.0,),
+            ),
+            // Existing Quick Access buttons
+            Padding(
+              padding: const EdgeInsets.only(top: 5.0),
               child: SizedBox(
                 child: ElevatedButton(
                   onPressed: () {},
                   style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all<Size>(const Size(double.infinity, 55)),
+                    minimumSize: MaterialStateProperty.all<Size>(
+                        const Size(double.infinity, 55)),
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(5),
@@ -77,16 +110,17 @@ class MyApp extends StatelessWidget {
                       color: Color(0xFFB0B0B0),
                     ),
                   ),
-                  ),
                 ),
               ),
-              Padding(
-              padding: const EdgeInsets.only(top: 5.0,),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 5.0),
               child: SizedBox(
                 child: ElevatedButton(
                   onPressed: () {},
                   style: ButtonStyle(
-                    minimumSize: MaterialStateProperty.all<Size>(const Size(double.infinity, 55)),
+                    minimumSize: MaterialStateProperty.all<Size>(
+                        const Size(double.infinity, 55)),
                     shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                       RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(5),
@@ -103,101 +137,38 @@ class MyApp extends StatelessWidget {
                       color: Color(0xFFB0B0B0),
                     ),
                   ),
-                  ),
                 ),
               ),
-              ],
             ),
-          )
-        ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            const Spacer(), // Add Spacer to push content up
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Make Reservation',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: 'View Reservations',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: const Color(0xFF1E80ED),
+        unselectedItemColor: const Color(0xFFB0B0B0),
+        backgroundColor: Colors.black, // Set the background color to black
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
